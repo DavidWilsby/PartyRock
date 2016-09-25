b73edc1f14bab6a5f2df952bb751a8750e2bdec3
MeatballMan <david.wilsby@gmail.com>
2016-09-25 04:51:53 +0200

    Added Graphics and VideoVC
    Graphics, Font ( Still Time ) and second viewController.

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by David Wilsby on 2016-09-22.
+//  Copyright © 2016 David Wilsby. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
+ 
